2b62153afce3607da3e97a572d635adf5bc66d97
ankjai <ankit.jaiswal@appdirect.com>
2016-02-24 10:29:32 -0800

    creating views and updating trig func.

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -46,7 +46,8 @@ loser_player_id INTEGER NOT NULL REFERENCES player(player_id)
 -- standing table
 CREATE TABLE standing(
 player_id INTEGER NOT NULL REFERENCES player ON DELETE CASCADE,
-player_win_count INTEGER NOT NULL DEFAULT 0
+player_win_count INTEGER NOT NULL DEFAULT 0,
+player_matches_count INTEGER NOT NULL DEFAULT 0
 );
 
 
@@ -66,8 +67,12 @@ CREATE OR REPLACE FUNCTION func_update_standing() RETURNS TRIGGER AS $trig_updat
 BEGIN
     IF (TG_OP = 'INSERT') THEN
         UPDATE standing
-            SET player_win_count = player_win_count + 1
+            SET player_win_count = player_win_count + 1,
+            player_matches_count = player_matches_count + 1
         WHERE player_id = NEW.winner_player_id;
+        UPDATE standing
+            SET player_matches_count = player_matches_count + 1
+        WHERE player_id = NEW.loser_player_id;
         RETURN NEW;
     END IF;
 END;
@@ -88,4 +93,20 @@ FOR EACH ROW EXECUTE PROCEDURE func_update_standing();
 
 
 -- CREATE VIEWS
---CREATE OR REPLACE VIEW vw_
+-- vw_player_details
+CREATE OR REPLACE VIEW vw_player_details AS
+SELECT t.tournament_id, t.tournament_name, p.player_id, p.player_name, s.player_win_count
+FROM tournament t
+JOIN player p ON
+t.tournament_id = p.tournament_id
+JOIN standing s ON
+p.player_id = s.player_id;
+
+-- vw_game_details
+CREATE OR REPLACE VIEW vw_game_details AS
+SELECT t.tournament_id, t.tournament_name, g.game_id, g.first_player_id, g.second_player_id, o.game_draw, o.winner_player_id, o.loser_player_id
+FROM tournament t
+JOIN game g ON
+t.tournament_id = g.tournament_id
+JOIN outcome o ON
+o.game_id = g.game_id;
